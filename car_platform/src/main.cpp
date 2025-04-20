76afd005d9e2617803110b0e924c4289e38c6352
140190 <1401906325@qq.com>
2025-04-20 15:07:29 +0800

    test

--- car_platform/src/main.cpp
+++ car_platform/src/main.cpp
@@ -4,12 +4,9 @@ const int ledPin = 36;
 
 void setup() {
   // 将 ledPin 配置为输出模式
-  pinMode(ledPin, OUTPUT); 
+
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);  // 点亮 LED（HIGH 表示高电平）
-  delay(1000);                // 延迟 1 秒
-  digitalWrite(ledPin, LOW);   // 熄灭 LED（LOW 表示低电平）
-  delay(1000);                // 延迟 1 秒
+
 }
